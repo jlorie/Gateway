c6058f7e5dd0da8681ff17320b627d4a8f07918e
Javier Lorié Guerra <jlorie87@gmail.com>
2014-03-19 18:12:30 -0430

    corregidos detalles de implementacion

--- DataServer/serialport/qtudev_p.h
+++ DataServer/serialport/qtudev_p.h
@@ -109,9 +109,9 @@ inline void *resolveSymbol(const char *symbolName)
 inline bool resolveSymbols()
 {
     if (!udevLibrary.isLoaded()) {
-        udevLibrary.setFileNameAndVersion(QStringLiteral("udev"), 1);
+        udevLibrary.setFileNameAndVersion(QString("udev"), 1);
         if (!udevLibrary.load()) {
-            udevLibrary.setFileNameAndVersion(QStringLiteral("udev"), 0);
+            udevLibrary.setFileNameAndVersion(QString("udev"), 0);
             if (!udevLibrary.load()) {
                 qWarning("Failed to load the library: %s, supported version(s): %i and %i", qPrintable(udevLibrary.fileName()), 1, 0);
                 return false;
